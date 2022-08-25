bc14b7240f7f4e6563e7a3782bf7172a03b00974
ShauryaAttal <87157875+ShauryaAttal@users.noreply.github.com>
2022-08-25 18:50:07 -0500

    Add files via upload

---
+++ Project-114/test1.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a number: "))
+b = int(input(" Enter another number: "))
+
+if a == b:
+    print("A and B are Equal")
+elif a > b:
+    print("A is greater than B")
+elif a < b:
+    print("A is less than B")
